cfbe9bdf0948837fc1c8eafd90b4d1bbb81d7b1d
ViniciusMelz <viniciusgmelz@gmail.com>
2024-09-04 22:08:50 -0300

    feat(CriarEvento): criada e implementada tela para cadastrar eventos

--- src/controllers/EventoController.php
+++ src/controllers/EventoController.php
@@ -52,59 +52,12 @@ class EventoController
         return $buscarEventos;
     }
 
-    /* public static function formInserirMarca() : void {
-        $acao = '/mvc/carros/marcas/inserir';
-        $funcao = 'Cadastro de Marcas';
+    public static function inserirEvento(string $tituloEvento, string $descricaoEvento, string $localEvento, string $dataEvento): bool
+    {
+        require 'src/models/EventoModel.php';
 
-        require ('src/views/marcaForm.php');
+        $model = new EventoModel();
+        $inserirEvento = $model->inserirEvento($tituloEvento, $descricaoEvento, $localEvento, $dataEvento);
+        return $inserirEvento;
     }
-
-    public static function formAlterarMarca() : void {
-        if(($_SERVER['REQUEST_METHOD'] === 'POST') && (isset($_POST['id']) )&& (isset($_POST['nome']))){
-            $acao = '/mvc/carros/marcas/alterar';
-            $id = $_POST['id'];
-            $nome = $_POST['nome'];
-            $funcao = 'Alteração da marca '.$nome;
-
-            require ('src/views/marcaForm.php');
-        }
-    }
-
-    public static function inserirMarca() : void {
-        if(($_SERVER['REQUEST_METHOD'] === 'POST') && (isset($_POST['nome']))){
-
-            require('src/models/MarcaModel.php');
-            $marca = new MarcaModel();
-            $retornoInserir = $marca->inserirMarca($_POST['nome']);
-
-            header('location: /mvc/carros/marcas/listar');
-        }else{
-            echo 'Ocorreu um erro ao inserir a marca';
-        }
-        
-    }
-
-    public static function excluirMarca() : void {
-        if(($_SERVER['REQUEST_METHOD']=== 'POST') && (isset($_POST['id']))){
-            require('src/models/MarcaModel.php');
-            $marca = new MarcaModel();
-            $retornoExcluir = $marca->excluirMarca($_POST['id']);
-
-            header('location: /mvc/carros/marcas/listar');
-        }else{
-            echo 'Ocorreu um erro ao excluir a marca';
-        }
-    }
-
-    public static function alterarMarca() : void {
-        if(($_SERVER['REQUEST_METHOD']=== 'POST') && (isset($_POST['id']) && (isset($_POST['nome'])))){
-            require('src/models/MarcaModel.php');
-            $marca = new MarcaModel();
-            $retornoExcluir = $marca->alterarMarca($_POST['id'], $_POST['nome']);
-
-            header('location: /mvc/carros/marcas/listar');
-        }else{
-            echo 'Ocorreu um erro ao excluir a marca';
-        }
-    } */
 }
---
+++ src/views/CriarEvento.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+if (!isset($_SESSION["usuario"])) {
+    header('location: /eventosWeb/Login');
+} else {
+    $erro = '';
+    $tituloEvento = $descricaoEvento = $localEvento = $dataEvento = "";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $tituloEvento = $_POST['tituloEvento'];
+        $descricaoEvento = $_POST['descricaoEvento'];
+        $localEvento = $_POST['localEvento'];
+        $dataEvento = $_POST['dataEvento'];
+
+        require_once "src/controllers/EventoController.php";
+        $eventoController = new EventoController();
+        $insercaoEvento = $eventoController->inserirEvento($tituloEvento, $descricaoEvento, $localEvento, $dataEvento);
+        if($insercaoEvento) {
+            ?>
+            <div id="aviso">
+                <?php echo 'Evento Cadastrado com Sucesso!' ?>
+            </div>
+            <script>
+                const aviso = document.getElementById('aviso');
+                aviso.style.display = 'block';
+                setTimeout(function () {
+                    aviso.style.display = 'none';
+                }, 5000);
+            </script>
+            <?php
+            $tituloEvento = $descricaoEvento = $localEvento = $dataEvento = "";
+        } else {
+            $erro = 'Erro ao cadastrar Evento, Tente novamente!';
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Jubileu Eventos</title>
+    <link rel="stylesheet" type="text/css" href="src/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+</head>
+
+<body>
+    <div class="flex-container">
+        <div id="containerLogin">
+            <h1 id="tituloLogin">CADASTRO DE EVENTO</h1>
+            <form action="" method="POST">
+                <input type="text" name="tituloEvento" value="<?php echo $tituloEvento ?>" required>
+                <input type="text" name="descricaoEvento" value="<?php echo $descricaoEvento ?>">
+                <input type="text" name="localEvento" value="<?php echo $localEvento ?>">
+                <input type="date" name="dataEvento" value="<?php echo $dataEvento ?>">
+                <input type="submit" value="Criar Evento">
+            </form>
+            <p id="erro"><?php echo $erro ?></p>
+            <a href="Eventos"><button>Voltar aos Eventos</button></a>
+        </div>
+    </div>
+</body>
+
+</html>
